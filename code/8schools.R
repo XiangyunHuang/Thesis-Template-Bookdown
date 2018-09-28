8c9ec96ee546cdda3a2bb5967517e77ff5729c7a
Xiangyun Huang <xiangyunfaith@outlook.com>
2018-09-28 19:46:43 +0800

    try different prior

---
+++ code/8schools.R
@@ -0,0 +1,32 @@
+
+schools_sim <- extract(fit, permuted = TRUE)
+
+pdf(file = "mu_tau.pdf", width = 8, height = 4)
+par(mfrow = c(1, 2), mar = c(4, 4, 2, 2))
+hist(schools_sim$mu, col = "lightblue", border = "white")
+abline(v = mean(schools_sim$mu), col = "darkorange", lwd = 2)
+hist(schools_sim$tau, col = "lightblue", border = "white")
+abline(v = mean(schools_sim$tau), col = "darkorange", lwd = 2)
+dev.off()
+
+pdf(file = "pairs_mu_tau_lp.pdf", width = 8, height = 6)
+pairs(fit, pars = c("mu", "tau", "lp__"), log = TRUE, las = 1) # tau will have logarithmic axes
+dev.off()
+
+# trace plot 追踪迭代过程
+params_tau <- as.data.frame(extract(fit, permuted=FALSE))
+names(params_tau) <- gsub("chain:1.", "", names(params_tau), fixed = TRUE)
+names(params_tau) <- gsub("[", ".", names(params_tau), fixed = TRUE)
+names(params_tau) <- gsub("]", "", names(params_tau), fixed = TRUE)
+params_tau$iter <- 1:5000
+
+pdf(file = "trace_log_tau.pdf", width = 8, height = 6)
+par(mar = c(4, 4, 0.5, 0.5))
+plot(params_tau$iter, log(params_tau$tau), col="darkorange", pch=16, cex=0.8,
+     xlab="Iteration", ylab="log(tau)",ylim = c(-6,4))
+dev.off()
+
+# 热身后，发散序列占比
+divergent <- get_sampler_params(fit, inc_warmup=FALSE)[[1]][,'divergent__']
+sum(divergent)
+sum(divergent) / 5000
--- code/8schools.stan
+++ code/8schools.stan
@@ -16,6 +16,9 @@ transformed parameters {
   // theta = mu + tau*eta;
 }
 model {
+  // set prior for mu or uniform prior distribution default
+  // target += normal_lpdf(mu  | 0, 10); 
+  // target += cauchy_lpdf(tau | 0, 25); # the same as mu
   target += normal_lpdf(eta | 0, 1);
   target += normal_lpdf(y | theta, sigma); // target distribution
   // y ~ normal(theta, sigma);
