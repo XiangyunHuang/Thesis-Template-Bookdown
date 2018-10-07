e56b217cfc62ba9ff94f932957f3a19a1ef0312a
Xiangyun Huang <xiangyunfaith@outlook.com>
2018-10-07 22:12:42 +0800

    compile stan model locally

--- code/pdf2png.R
+++
@@ -1,7 +0,0 @@
-# PDF 转 PNG 格式
-# convert -quality 100 -density 300x300 figures/matern.pdf figures/matern.png
-# convert -quality 100 -density 300x300 figures/map-loaloa.pdf figures/map-loaloa.png
-
-# 优化 PNG 图片
-
-# optipng -o5 figures/*.png
---
+++ code/pdf2png.sh
@@ -0,0 +1,8 @@
+# PDF 转 PNG 格式
+convert -quality 100 -density 300x300 figures/matern.pdf figures/matern.png
+convert -quality 100 -density 300x300 figures/map-loaloa.pdf figures/map-loaloa.png
+
+# 优化 PNG 图片
+
+optipng -o5 figures/*.png
+convert -quality 100 -density 300x300 figures/one-dim-gp.pdf figures/one-dim-gp.png
